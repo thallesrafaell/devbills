0f1149bc6cf27f5cdeb07d0773aa5605921c6ded
Thalles <thallesrafaell@outlook.com>
2025-06-17 20:54:36 -0300

    feat: atualiza o componente Footer para exibir o ano atual e informações sobre a tecnologia utilizada

--- interface/src/components/footer/index.tsx
+++ interface/src/components/footer/index.tsx
@@ -1,7 +1,17 @@
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <footer>
-      <p>© 2025 - Todos os direitos reservados</p>
+      <p className="bg-gray-800 text-sm font-medium text-center text-gray-400 py-2 flex justify-center items-center flex-col gap-2 border-t border-gray-400 boder-solid md:flex-row">
+        <span className="text-gray-400">
+          <span className="font-bold text-primary-500">DevBills</span> ©{' '}
+          {currentYear}
+        </span>{' '}
+        <span className="hidden md:inline">—</span>
+        <span className="text-gray-400">
+          Desenvolvido com React, TypeScript e Tailwind CSS
+        </span>
+      </p>
     </footer>
   );
 };
